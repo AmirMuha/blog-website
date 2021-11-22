185d738a7177824cbc7cf03d0202d7f670c65b7c
amirmuha <mirzaii4046@gmail.com>
2021-11-22 22:55:18 +0330

    feat: customized theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,38 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+import "../styles/tailwindcss.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {ThemeProvider, createTheme, ThemeOptions} from "@mui/material/styles";
+
+const themeOptions: ThemeOptions = {
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#ff6f00",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+    background: {
+      default: "#370909",
+      paper: "#672626",
+    },
+    warning: {
+      main: "#f34f00",
+    },
+  },
+};
+const theme = createTheme(themeOptions);
+
+interface Props extends AppProps {
+  news: any[];
+}
+function MyApp({ Component, pageProps, news }: Props) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
