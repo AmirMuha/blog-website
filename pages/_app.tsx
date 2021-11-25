7274c5d8099cec61b4d9f788f03a584199ab2424
amirmuha <mirzaii4046@gmail.com>
2021-11-25 21:36:37 +0330

    remove: removed custom theme configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,16 @@
-import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import "../styles/tailwindcss.css";
+import type { AppProps } from "next/app";
 import CtxProvider from "../store/context/topics"
 
-import {ThemeProvider, createTheme, ThemeOptions} from "@mui/material/styles";
-
-const themeOptions: ThemeOptions = {
-  palette: {
-    mode: "dark",
-    primary: {
-      main: "#ff6f00",
-    },
-    secondary: {
-      main: "#f50057",
-    },
-    background: {
-      default: "#370909",
-      paper: "#672626",
-    },
-    warning: {
-      main: "#f34f00",
-    },
-  },
-};
-const theme = createTheme(themeOptions);
 
 interface Props extends AppProps {
   news: any[];
 }
 function MyApp({ Component, pageProps, news }: Props) {
   return (
-    <ThemeProvider theme={theme}>
     <CtxProvider>
       <Component {...pageProps} />
-    </ThemeProvider>
     </CtxProvider>
   );
 }
